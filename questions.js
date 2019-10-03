dcdf1ad7cc4ead4f7550f6624db6d0fe578b8eeb
MahaLodu18 <56104395+MahaLodu18@users.noreply.github.com>
2019-10-03 10:53:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'Who is the founder of apple computers?': [
+        'Steve jobs',
+        'Steve Wozniak',
+        'Bill gates',
+      ],
+    },
+    {
+      'Who is the founder of facebook?': [
+        'Mark Zuckerberg',
+        'Dustin hoffman',
+        'Edvardo savren',
+      ],
+    },
+    {
+      'In which year was facebook founded?': [
+        '2005',
+        '2010',
+        '1997',
+      ],
+    },
+    {
+      'Which one is a popular photo sharing application?': [
+        'Instagram',
+        'gmail',
+        'yahoo',
+      ],
+    },
+    {
+      'Who is the CEO of mircosoft?': [
+        'Satya nadella',
+        'Indira nooyi',
+        'Joseph smith',
+      ],
+    },
+  ],
+  QUESTIONS_EN_US: [
+    {
+      'Who is the founder of apple?': [
+        'Steve jobs',
+        'Steve Wozniak',
+        'Bill gates',
+      ],
+    },
+    {
+      'Who is the founder of facebook?': [
+        'Mark Zuckerberg',
+        'Dustin hoffman',
+        'Edvardo savren',
+      ],
+    },
+    {
+      'In which year was facebook founded?': [
+        '2005',
+        '2010',
+        '1997',
+      ],
+    },
+    {
+      'Which one is a popular photo sharing application?': [
+        'Instagram',
+        'gmail',
+        'yahoo',
+      ],
+    },
+    {
+      'Who is the CEO of mircosoft?': [
+        'Satya nadella',
+        'Indira nooyi',
+        'Joseph smith',
+      ],
+    },
+  ],
+};
